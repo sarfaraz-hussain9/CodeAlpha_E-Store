a785a406d678f0d2611b1cbe8ece54ddb1985d3f
sarfaraz-hussain9 <145418136+sarfaraz-hussain9@users.noreply.github.com>
2024-07-19 19:28:49 +0530

    Update apiSlice.js

--- frontend/src/redux/api/apiSlice.js
+++ frontend/src/redux/api/apiSlice.js
@@ -1,8 +1,6 @@
 import {fetchBaseQuery,createApi} from "@reduxjs/toolkit/query/react"
 
-import { BASE_URL } from "../features/constants"
-
-const baseQuery=fetchBaseQuery({baseUrl:BASE_URL});
+const baseQuery=fetchBaseQuery({baseUrl:''});
 
 export const apislice=createApi({
     baseQuery,
