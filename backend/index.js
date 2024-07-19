f3a737fd169d2b8bb1d3052698bfa20b34f292ec
sarfaraz-hussain9 <145418136+sarfaraz-hussain9@users.noreply.github.com>
2024-07-19 18:34:40 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,34 +10,41 @@ import cors from "cors"
 import connectDB from "./utils/db.js"
 import userRoute from "./routes/userRoute.js"
 import productRoute from "./routes/productRoute.js"
-
 import uploadRoute from "./routes/uploadRoute.js"
 
 dotenv.config();
 
+const app=express();
 const port=process.env.PORT || 8000;
 
 connectDB();
 
-const app=express();
+app.use(cors({
+  origin: 'https://your-frontend-domain.onrender.com', // Replace with your actual frontend URL
+  optionsSuccessStatus: 200,
+}));
 
 const __dirname = path.resolve();
 
-app.use(cors());
+
+
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 app.use("/api/v1",userRoute)
 app.use("/api/v1/product",productRoute)
 app.use("/api/v1/upload",uploadRoute)
 
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 app.use('/uploads',express.static(path.join(__dirname,"./uploads")))
 
-app.use(express.static(__dirname));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../frontend/dist/index.html'));
+});
+
 
 app.listen(port,()=>{
     console.log(`server is running on port: ${port}`)
