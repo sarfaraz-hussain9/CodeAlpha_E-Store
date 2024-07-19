659632fbb9e380032a442153c2929aa5223c27a3
sarfaraz-hussain9 <145418136+sarfaraz-hussain9@users.noreply.github.com>
2024-07-19 20:19:10 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,12 @@ import express from 'express';
 import path from 'path';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
-import cors from 'cors';
 import connectDB from './utils/db.js';
 import userRoute from './routes/userRoute.js';
 import productRoute from './routes/productRoute.js';
 import uploadRoute from './routes/uploadRoute.js';
 
+// Load environment variables from .env file
 dotenv.config();
 
 const app = express();
@@ -15,7 +15,6 @@ const port = process.env.PORT || 8000;
 
 connectDB();
 
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -25,17 +24,23 @@ app.use('/api/v1', userRoute);
 app.use('/api/v1/product', productRoute);
 app.use('/api/v1/upload', uploadRoute);
 
-if(process.env.NODE_ENV==='production'){
-    const __dirName=path.resolve();
-    app.use(express.static(path.join(__dirName,'/frontend/dist')));
+// Serve static files from the React frontend in production
+if (process.env.NODE_ENV === 'production') {
+    const __dirname = path.resolve();
+    
+    // Serve frontend static files
+    app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
 
-    app.use('/uploads', express.static(path.join(__dirname, './uploads')));
-    app.get('*',(req,res)=>res.sendFile(path.resolve(__dirName,'frontend','dist','index.html')))
-}else{
-    app.get('/',(req,res)=> res.send('Server is Ready'))
+    // Serve uploaded files
+    app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+    // Handle all other routes
+    app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html')));
+} else {
+    app.get('/', (req, res) => res.send('Server is Ready'));
 }
 
-
+// Start the server
 app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
+    console.log(`Server is running on port: ${port}`);
 });
