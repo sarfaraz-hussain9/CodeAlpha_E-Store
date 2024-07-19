88a5b88ffcc114ea888d4de7f045323bfb227001
sarfaraz-hussain9 <145418136+sarfaraz-hussain9@users.noreply.github.com>
2024-07-19 19:02:41 +0530

    Update constants.js

--- frontend/src/redux/features/constants.js
+++ frontend/src/redux/features/constants.js
@@ -1,4 +1,4 @@
 export const BASE_URL = ""
-export const USERS_URL="https://mern-estore-k21u.onrender.com/api/v1"
-export const PRODUCT_URL="https://mern-estore-k21u.onrender.com/api/v1/product"
-export const UPLOAD_URL="https://mern-estore-k21u.onrender.com/api/v1/upload"
+export const USERS_URL="https://mern-estore-ayq6.onrender.com/api/v1"
+export const PRODUCT_URL="https://mern-estore-ayq6.onrender.com/api/v1/product"
+export const UPLOAD_URL="https://mern-estore-ayq6.onrender.com/api/v1/upload"
